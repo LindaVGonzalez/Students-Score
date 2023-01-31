9c47f7ee15a42d257c7495838188a7ded0f83883
LindaVGonzalez <66371465+LindaVGonzalez@users.noreply.github.com>
2023-01-31 21:39:35 +0000

    Added Student_scores.py

---
+++ Student_scores.py
@@ -0,0 +1,18 @@
+name = input("Please enter your full name:")
+
+print("Please "+str(name)+" enter your score on each of the subjects below ")
+
+score1 = int(input("ICT:"))
+score2 = int(input("MATHS:"))
+score3 = int(input("CHEMISTRY:"))
+
+
+percentage = (score1 + score2 + score3) /3
+print("Your academic score is: "+str(percentage)+" % ")
+
+
+
+
+
+
+      
