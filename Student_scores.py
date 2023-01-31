a2430d7dda66f5cad7ced92cfedf9376e4a19f6e
LindaVGonzalez <66371465+LindaVGonzalez@users.noreply.github.com>
2023-01-31 20:21:40 +0000

    Added README.md

---
+++ Student_scores.py
@@ -0,0 +1,7 @@
+name = input("Please enter your name:")
+
+print("Please "+name" enter your score on each of the subjects below ")
+
+
+
+      
---
+++ studentprojecttest.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="root"
+)
+
+print(mydb)
+
+
+
+
+
+
+
+ 
